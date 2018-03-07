842931228b0163449e1bdd8375bc52a22e6701a8
karenmudi17 <37043272+karenmudi17@users.noreply.github.com>
2018-03-07 15:47:41 +0100

    home work

---
+++ MyFirst.java
@@ -0,0 +1,14 @@
+public class MyFirst {
+
+ public static void main (String[] args) {
+//  System.out.println("Hello, World!");
+
+  public static double distance(Point p1, Point p2) {
+     return Math.sqrt(math.pow((p1.x - p2.x),2)+Math.pow((p1.y-p2.y),2);
+ }
+
+  Point p1 = new Point (3,5);
+  Point p2 = new Point (1,2);
+  System.out.println("Расстояние между точками" + distance(p1,p2));
+  }
+}

8823e2370075efe85ae56566eb2eec43bf5cbce8
Anya Kriuchkova <karenmudi17@gmail.com>
2018-03-17 11:25:00 +0100

    New group creation test

---
+++ addressbook-web-tests/src/test/java/study/addressbook/GroupCreationTests.java
@@ -0,0 +1,80 @@
+package study.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.*;
+
+public class GroupCreationTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/index.php");
+        login("admin", "secret");
+    }
+
+    private void login(String login, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(login);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testGroupCreation() {
+        gotoNewGroup();
+        initNewGroupCreation("new");
+        fillGroupForm(new GroupData("studyGroup2", "studytest2", "study2"));
+        submitGroupCreation("submit");
+        gotoNewGroup();
+    }
+
+    private void submitGroupCreation(String submit) {
+        wd.findElement(By.name(submit)).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initNewGroupCreation(String s) {
+        wd.findElement(By.name(s)).click();
+    }
+
+    private void gotoNewGroup() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}

68a6bcc374de53800625c3583b3218d8bd86eb3c
Anya Kriuchkova <karenmudi17@gmail.com>
2018-04-18 15:29:42 +0200

    Groups sorting

--- addressbook-web-tests/src/test/java/study/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/study/addressbook/tests/GroupModificationTests.java
@@ -4,8 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import study.addressbook.model.GroupData;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GroupModificationTests extends TestBase{
 
@@ -27,6 +30,9 @@ public class GroupModificationTests extends TestBase{
 
         before.remove(before.size() - 1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1,g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }

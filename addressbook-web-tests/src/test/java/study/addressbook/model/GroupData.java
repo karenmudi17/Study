6dce467e7404fd8b6f4519cd23bebb066e4cb474
Anya Kriuchkova <karenmudi17@gmail.com>
2018-04-18 14:39:25 +0200

    Groups Creation checks are added

--- addressbook-web-tests/src/test/java/study/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/study/addressbook/model/GroupData.java
@@ -3,15 +3,27 @@ package study.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String name, String header, String footer) {
+
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
+    public GroupData(String name, String header, String footer) {
+        this.id = 0;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getName() {
         return name;
@@ -29,6 +41,7 @@ public class GroupData {
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
+                ", id='" + id + '\'' +
                 '}';
     }
 
@@ -37,12 +50,13 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name);
+        return Objects.hash(name, id);
     }
 }
--- addressbook-web-tests/src/test/java/study/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/study/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import study.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -12,15 +13,20 @@ public class GroupModificationTests extends TestBase{
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("studyGroup2", null, "study2"));
+            app.getGroupHelper().createGroup(new GroupData("studyGroup2", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 2);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("studyModif", "aaa", "bbb"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"studyGroup2","test2","test3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(),before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
